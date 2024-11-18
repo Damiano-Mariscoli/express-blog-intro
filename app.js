00c2aa0da3e370639dabbead849ab689a74aaabf
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-18 16:24:21 +0100

    ADDED: ritorno del file json e count dei post

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const posts = [
+  { title: "", content: "", img: "", tags: ["", "", ""] },
+  { title: "", content: "", img: "", tags: ["", "", ""] },
+  { title: "", content: "", img: "", tags: ["", "", ""] },
+  { title: "", content: "", img: "", tags: ["", "", ""] },
+  { title: "", content: "", img: "", tags: ["", "", ""] },
+];
 
-app.listen(port , () =>{
-    console.log('server')
-})
+app.get("/bacheca", (req, res) => {
+    const postCount = posts.length;
+    res.json({ posts: posts, count: postCount });
+});
+
+app.listen(port, () => {
+  console.log("server");
+});
