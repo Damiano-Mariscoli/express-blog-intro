d08e2828893b0088e36d2bc613df52494dbe1043
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-18 16:29:38 +0100

    ADDED: percorsi imgs statici

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const posts = [
-  { title: "", content: "", img: "", tags: ["", "", ""] },
-  { title: "", content: "", img: "", tags: ["", "", ""] },
-  { title: "", content: "", img: "", tags: ["", "", ""] },
-  { title: "", content: "", img: "", tags: ["", "", ""] },
-  { title: "", content: "", img: "", tags: ["", "", ""] },
+  { title: "", content: "", img: "./public/images/ciambellone.jpeg", tags: ["", "", ""] },
+  { title: "", content: "", img: "./public/images/cracker_barbabietola.jpeg", tags: ["", "", ""] },
+  { title: "", content: "", img: "./public/images/pane_fritto_dolce.jpeg", tags: ["", "", ""] },
+  { title: "", content: "", img: "./public/images/pasta_barbabietola.jpeg", tags: ["", "", ""] },
+  { title: "", content: "", img: "./public/images/torta_paesana.jpeg", tags: ["", "", ""] },
 ];
+
+
+app.use(express.static('public'))
 
 app.get("/bacheca", (req, res) => {
     const postCount = posts.length;
